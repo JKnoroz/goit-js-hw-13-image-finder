a828c383b23b4ed2c32283cc415dc11ffcf18e5b
Yuliya <jknoroz@gmail.com>
2021-06-15 21:08:28 +0300

    4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './sass/main.scss';
 import './js/search';
 import './js/apiService';
+import '../node_modules/material-design-icons/index';
--- src/js/search.js
+++ src/js/search.js
@@ -2,7 +2,7 @@ import PixaApiService from './apiService';
 import galleryCardTpl from '../partials/photo-card.hbs';
 
 const searchForm = document.querySelector('#search-form');
-const articlesContainer = document.querySelector('.gallery');
+const imgsContainer = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('[data-action="load-more');
 console.log(loadMoreBtn);
 
@@ -14,7 +14,14 @@ loadMoreBtn.addEventListener('click', onLoadMore);
 function onSearch(e) {
   e.preventDefault();
 
+  clearImgsContainer();
+
   pixaApiService.query = e.currentTarget.elements.query.value;
+
+  if (pixaApiService.query === '') {
+    return console.log('NO');
+  }
+
   pixaApiService.resetPage();
   pixaApiService.fetchImages().then(appendImgsMarkup);
 }
@@ -24,5 +31,9 @@ function onLoadMore() {
 }
 
 function appendImgsMarkup(hits) {
-  articlesContainer.insertAdjacentHTML('beforeend', galleryCardTpl(hits));
+  imgsContainer.insertAdjacentHTML('beforeend', galleryCardTpl(hits));
+}
+
+function clearImgsContainer() {
+  imgsContainer.innerHTML = '';
 }
